eade141fba9c446e89c336e11f2e7069fe166546
James Morelli <morellijames12@gmail.com>
2025-10-21 21:51:45 -0400

    Create blank component for movie description

---
+++ app/pages/movie_description/MovieDescription.tsx
@@ -0,0 +1,8 @@
+const MovieDescription = ({ movieId }: {movieId: string}) => {
+    return (
+        <div>
+            <h1>Movie Description Page {movieId}</h1>
+        </div>
+    );
+}
+export default MovieDescription;
