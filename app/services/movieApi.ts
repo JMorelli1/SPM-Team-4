3d759b1b9356637e187785c023b3de5321e1ed2e
James Morelli <morellijames12@gmail.com>
2025-11-09 17:31:23 -0500

    Integrate with the vercel proxy

--- app/services/movieApi.ts
+++ app/services/movieApi.ts
@@ -4,15 +4,34 @@ import type { MovieCredits } from "~/types/MovieCredits";
 import type { MovieDescription } from "~/types/MovieDescription";
 import type { MovieDiscover } from "~/types/MovieDiscover";
 
+const isLocal = import.meta.env.VITE_IS_LOCAL ?? false;
+const proxyURL = 'https://spm-team-4-mgv2.vercel.app/api/proxy';
 const apiBaseUrl = '/api/movies';
 
 const defaultHeaders = {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${import.meta.env.VITE_TMDB_API_KEY}`
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${import.meta.env.VITE_TMDB_API_KEY}`
+}
+
+const buildUrl = (path: string, queryParams?: Record<string, any>) => {
+    const baseUrl = isLocal ? `${apiBaseUrl}${path}` : `${proxyURL}` // Set proxy when non local
+    const params = buildQueryParams(queryParams ?? {}, path)
+
+    return `${baseUrl}${params ? `?${params}`: ''}`
+}
+
+const buildQueryParams = (params: Record<string, any>, path: string) => {
+    const urlParams = new URLSearchParams();
+    if (!isLocal) {
+        urlParams.append("path", path);
+    }
+
+    Object.entries(params).forEach(([key, value]) => urlParams.append(key, value))
+    return urlParams;
 }
 
 export const fetchDiscoverMovies = async (query: string, page: number = 1) => {
-    const response = await fetch(`${apiBaseUrl}/search/multi?query=${query}&page=${page}`, {
+    const response = await fetch(buildUrl('/search/multi', { "query": query, "page": page }), {
         method: 'GET',
         headers: defaultHeaders,
     });
@@ -20,7 +39,7 @@ export const fetchDiscoverMovies = async (query: string, page: number = 1) => {
 }
 
 export const fetchMovieById = async (movieId: number | string): Promise<MovieDescription> => {
-    const response = await fetch(`${apiBaseUrl}/movie/${movieId}`, {
+    const response = await fetch(buildUrl(`/movie/${movieId}`), {
         method: 'GET',
         headers: defaultHeaders,
     });
@@ -29,7 +48,7 @@ export const fetchMovieById = async (movieId: number | string): Promise<MovieDes
 
 
 export const fetchActorById = async (actorId: number | string): Promise<ActorDescription> => {
-    const response = await fetch(`${apiBaseUrl}/person/${actorId}`, {
+    const response = await fetch(buildUrl(`/person/${actorId}`), {
         method: 'GET',
         headers: defaultHeaders,
     });
@@ -37,7 +56,7 @@ export const fetchActorById = async (actorId: number | string): Promise<ActorDes
 }
 
 export const fetchMovieCreditsById = async (movieId: number | string): Promise<MovieCredits> => {
-    const response = await fetch(`${apiBaseUrl}/movie/${movieId}/credits`, {
+    const response = await fetch(buildUrl(`/movie/${movieId}/credits`), {
         method: 'GET',
         headers: defaultHeaders,
     });
@@ -45,7 +64,7 @@ export const fetchMovieCreditsById = async (movieId: number | string): Promise<M
 }
 
 export const fetchActorCreditsById = async (actorId: number | string): Promise<ActorCredits> => {
-    const response = await fetch(`${apiBaseUrl}/person/${actorId}/movie_credits`, {
+    const response = await fetch(buildUrl(`/person/${actorId}/movie_credits`), {
         method: 'GET',
         headers: defaultHeaders,
     });
@@ -53,16 +72,7 @@ export const fetchActorCreditsById = async (actorId: number | string): Promise<A
 }
 
 export const fetchMovieBannerDiscover = async (genre?: string): Promise<MovieDiscover> => {
-    if (!genre) {
-        genre = 'action'
-    }
-
-    const params = new URLSearchParams();
-    params.append('genre', genre);
-    params.append('page', '1');
-    params.append('sort_by', 'popularity.desc')
-
-    const response = await fetch(`${apiBaseUrl}/discover/movie?${params}`, {
+    const response = await fetch(buildUrl('/discover/movie', { 'genre': genre ?? 'action', 'page': '1', 'sort_by': 'popularity.desc' }), {
         method: 'GET',
         headers: defaultHeaders,
     });
